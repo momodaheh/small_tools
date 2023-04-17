7744ced6af6702ac8a2e8e91c79b93aab16b3a7b
momoda <WZY450416@outlook.com>
2023-04-17 21:20:58 +0800

    feat:添加图片资源

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-import "./App.css";
+
+import styles from "./App.css";
+import logo from "../src/assets/img/logo.jpg"
 
 const menuitems = [
   {
-    name: 'buff低价提醒',
+    name: "buff低价提醒",
     value: 1,
   },
   {
-    name: '自动生成图标',
+    name: "自动生成图标",
     value: 2,
   },
   {
-    name: '以撒的结合重生图层攻略',
+    name: "以撒的结合重生图层攻略",
     value: 3,
   },
 ];
 function App() {
   return (
-    <div>
-      <div className="bian"></div>
-      <div className="menu">
-        {menuitems.map((item, index) => {
-            return <div className="menuitem" key={index}>
-              {item.name}
-            </div>
-        })}
+    <div className={styles.all}>
+      <div className="bian">
+        <div>
+          <img src={logo} className="logo"></img>
         </div>
+      </div>
+      <div className="center">
+        <div className="menu">
+          {menuitems.map((item, index) => {
+            return (
+              <div className="menuitem" key={index}>
+                {item.name}
+              </div>
+            );
+          })}
+        </div>
+        <div className="content">
+
+        </div>
+      </div>
+
       <div className="bian"></div>
     </div>
   );
 }
-
 export default App;
