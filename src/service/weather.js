86b3d4896cfbd6fc4c7ece0c7af0c33dd49fcac5
momoda <WZY450416@outlook.com>
2023-04-25 00:33:05 +0800

    feat:weather页面citys

--- src/service/weather.js
+++ src/service/weather.js
@@ -1,6 +1,6 @@
 import http from "../utils/http";
 
-export const get_Date = async(city) => {
+export const get_Data = async(city) => {
     const ret = await http.get('https://api.seniverse.com/v3/weather/daily.json?key=SHNeyyo1Ab9YKrFDj&location='+city+'&language=zh-Hans&unit=c&start=0&days=5')
     return ret
 }
--- src/views/weather/index.jsx
+++ src/views/weather/index.jsx
@@ -1,20 +1,54 @@
-import React ,{ useState, useEffect }from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./index.module.scss";
-import { get_Date } from "../../service/weather";
-import { message } from "antd";
+import { get_Data } from "../../service/weather";
+import { Select, message } from "antd";
 
 export default function Weather() {
-  const [city,setcity] = useState("杭州")
-
+  const [city, setCity] = useState("杭州");
+  const [data, setData] = useState([]);
   useEffect(() => {
-    get_Date(city)
-    .then((ret) =>{
-      if(ret!=null){
-        console.log(ret);
-      }else{
-        message.error("获取数据失败")
+    get_Data(city).then((ret) => {
+      if (ret != null) {
+        setData(ret.data.results[0].daily);
+        console.log(ret.data.results[0].daily);
+      } else {
+        message.error("获取数据失败");
       }
-    })
-  },[city])
+    });
+  }, [city]);
 
+  const Citys = [
+    {
+      label: "杭州",
+      value: "hangzhou",
+    },
+    {
+      label: "上海",
+      value: "shanghai",
+    },
+    {
+      label: "北京",
+      value: "北京",
+    },
+  ];
+
+  const handleChange = (value) =>{
+    setCity(value); 
+  }
+
+  return (
+    <div>
+      <div className={styles.top_select} id="top">
+        <span>天气预报</span>
+        <Select
+          defaultValue="hangzhou"
+          style={{
+            width: 120,
+          }}
+          onChange={handleChange}
+          options={Citys}
+        />
+      </div>
+    </div>
+  );
 }
